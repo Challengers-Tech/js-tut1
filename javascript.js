667f207cb803ded7d72cb6cc90b221781d3ff06b
Justin Challenger <challenger.justin@gmail.com>
2021-01-08 19:12:51 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
 console.log("Hello World!");
+
+
+/*
+ * Ha, Data Types
+ * 
+ * 1872 - Number (Whole)
+ * 73.92 - Number (Float)
+ * "c" - Character 
+ * "Justin" - String (list of characters)
+ * 
+ * [] - Array (List of things)
+ * {} - Object
+ * 
+ * string[] 
+ */
+var apples = ["green", "yellow", "red"];
+console.log("Apple:" , apples[0]);
+
+// This is the first name variable
+var fname = "Justin";
+
+// This controls the age
+var age = 22;
+age = 23;
+console.log(age);
+
+age = fname;
+console.log(age);
