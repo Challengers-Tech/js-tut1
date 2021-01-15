58eb9c35c005cc56729c280ddbaae7f0cc779c53
Justin Challenger <challenger.justin@gmail.com>
2021-01-15 18:06:32 -0500

    describe the basic functions we'll use so far

--- javascript.js
+++ javascript.js
@@ -1,23 +1,48 @@
-console.log("Hello World!");
-
+// Store one line comments starting with //
+// Everything in a comment is ignored by the client
 
 /*
- * Ha, Data Types
- * 
- * 1872 - Number (Whole)
- * 73.92 - Number (Float)
- * "c" - Character 
- * "Justin" - String (list of characters)
- * 
- * [] - Array (List of things)
- * {} - Object
- * 
- * string[] 
- */
-var apples = ["green", "yellow", "red"];
-console.log("Apple:" , apples[0]);
 
-// This is the first name variable
+Start multi line comments  with /*
+when you type * / (without a space), it will end the comment.
+This means you can use it inline or multiline
+
+
+Data Types (the kinds of data we care about)
+ 1872 - Number (Whole)
+ 73.92 - Number (Float)
+ "c" - Character 
+ "Justin" - String (list of characters)
+ 
+ [] - Array (List of things)
+ {} - Object
+
+Math
+ We can use common mathamatical operators
+
+ + add ( use like `var a = x + y;` )
+ - subtract
+ * multiply
+ / divide 
+ % divide (remainder)
+
+*/
+
+// Variables and Assignment
+
+//You can create a new variable by declaring it
+var x;
+// once it exists, you can use it to store values
+x = 7;
+// then, you can do stuff with it, like
+x = x * 12345;
+
+// The console variable is always available, along with some other System provided libraries
+// We can use them to do common things, line print to the terminal window
+console.log("Our Variable X is: " + x);
+
+
+// We can assign to our variable when we declare it, and use it right away
 var fname = "Justin";
 
 // This controls the age
@@ -27,3 +52,10 @@ console.log(age);
 
 age = fname;
 console.log(age);
+
+
+// Comparators
+
+
+// IF CONDITIONS
+
